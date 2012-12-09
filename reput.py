e5d1fcffe3e1c3498f89f5ceaa323a5f3c27e9e3
Rahul Dalal <rahul@Rahuls-MacBook-Pro.local>
2012-12-08 23:52:35 -0500

    errors in actions!?!?!?!?!?!?!?!?!?!?!?!?

--- reput.py
+++ reput.py
@@ -8,6 +8,7 @@ import cPickle
 from multiprocessing import Process, Queue
 
 SEL = 1
+ERR = 0
 
 def wrand(weight):
     a = random.random() * weight.sum()
@@ -36,7 +37,7 @@ def interact(fit, opi, rep, stg, n, c, b):
     cur = np.zeros(opi.shape)
     for t in xrange(n):
         don, rec = random.sample(xrange(len(rep)), 2)
-        if rep[rec] > stg[don]:
+        if (rep[rec] > stg[don] and random.random > ERR) || (rep[rec] < stg[don] and random.random < ERR):
             cur[don][rec] += 1
             fit[don] -= c*SEL
             fit[rec] += b*SEL
