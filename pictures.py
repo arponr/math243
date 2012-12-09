069595673f688b357ecf27c733ad904a8f3a4e4d
Ben Kuhn <ben.s.kuhn@gmail.com>
2012-12-09 17:12:54 -0500

    show pictures in grid

--- pictures.py
+++ pictures.py
@@ -1,5 +1,7 @@
+from __future__ import division
 import matplotlib.pyplot as plt, sys
 import cPickle
+from math import *
 
 if __name__ == "__main__":
     outname = sys.argv[1]
@@ -12,10 +14,14 @@ if __name__ == "__main__":
                 if not k in results:
                     results[k] = []
                 results[k].extend(v)
-    w = 4
-    h = 3
-    for i in xrange(len(results['ast'])):
-        plt.plot(results['ast'][i])
-        plt.plot(results['arp'][i])
-        plt.show()
-    #plt.savefig(outname)
+    n = len(results['ast'])
+    w = int(ceil(sqrt(n)))
+    h = int(ceil(n/w))
+    for i in xrange(n):
+        plt.subplot(w,h,i)
+        plt.plot(results['ast'][i], label='avg. coop threshold')
+        plt.plot(results['arp'][i], label='avg. reputation')
+        plt.legend()
+    plt.savefig(outname,dpi=300)
+    plt.show()
+
