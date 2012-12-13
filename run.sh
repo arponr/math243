b4fb885bd27694c21d89f01b9fdb2c5e29b4f798
Rahul Dalal <rahul@Rahuls-MacBook-Pro.local>
2012-12-12 23:38:31 -0500

    population measuring

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ for MEETS in 10 20 50 100
 do
     echo MEETS=$MEETS
     python reput.py data$MEETS MEETS=$MEETS STEPS=25000 DUMP=2500 C=1\
-    B=2 N=100 PROC=4 SIM=16 
+    B=2 N=$MEETS PROC=4 SIM=16 
 done
 
 python morepics.py data '[10, 20, 50, 100]'
