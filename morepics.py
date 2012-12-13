df85808c9ae1ca5e5851535042402fd1f98c06f2
Ben Kuhn <ben.s.kuhn@gmail.com>
2012-12-13 02:50:00 -0500

    plot cooperations

--- morepics.py
+++ morepics.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     stg_x, stg_y = [], []
     rep_x, rep_y = [], []
     fit_x, fit_y = [], []
+    cps_x, cps_y = [], []
     weight = []
     for i, val in enumerate(vals):
         fname = name + str(val)
@@ -20,21 +21,26 @@ if __name__ == "__main__":
         with open(fname + '.out') as f:
             data = cPickle.load(f)
             params = data['params']
+            dump = params['DUMP']
             snaps = int(params['STEPS'] / params['DUMP'])
             trials = params['PROC'] * params['SIM'] 
             N = params['N']
+            meets = params['MEETS']
             for k in xrange(snaps):
                 if i == 0:
                     stg_x.append([])
                     rep_x.append([])
                     fit_x.append([])
+                    cps_x.append([])
                     stg_y.append([])
                     rep_y.append([])
                     fit_y.append([])
+                    cps_y.append([])
                     weight.append([])
                 stg_x[k].extend([i] * trials * N)
                 rep_x[k].extend([i] * trials * N)
                 fit_x[k].extend([i] * trials * N)
+                cps_x[k].extend([i] * trials)
                 if vary == 0:
                     weight.extend([1] * trials * N)
                 else:
@@ -43,8 +49,10 @@ if __name__ == "__main__":
                     stg_y[k].extend(data['pst'][j][k])
                     rep_y[k].extend(data['prp'][j][k])
                     fit_y[k].extend(data['pft'][j][k])
+                    cps_y[k].append(data['cps'][j][k * dump] / meets)
     for k in xrange(1, snaps):
         plt.figure()
-        plt.hexbin(rep_x[k], rep_y[k], weights[k], cmap=cm.binary, 
-                   gridsize=10, reduce_C_function=sum)
+        print cps_x[k], cps_y[k]
+        plt.hexbin(cps_x[k], cps_y[k], cmap=cm.binary, 
+                   gridsize=10)
         plt.savefig('data%d.png' % k, dpi=300)
