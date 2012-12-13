98d3a49874922965a9e5cdfaa5a5c3849c92641a
Arpon Raksit <arpon.raksit@gmail.com>
2012-12-13 02:03:00 -0500

    morepics save png, weights

--- morepics.py
+++ morepics.py
@@ -9,9 +9,11 @@ from math import *
 if __name__ == "__main__":
     name = sys.argv[1]
     vals = eval(sys.argv[2])
+    vary = sys.argv[3]
     stg_x, stg_y = [], []
     rep_x, rep_y = [], []
     fit_x, fit_y = [], []
+    weight = []
     for i, val in enumerate(vals):
         fname = name + str(val)
         data = {}
@@ -29,14 +31,20 @@ if __name__ == "__main__":
                     stg_y.append([])
                     rep_y.append([])
                     fit_y.append([])
+                    weight.append([])
                 stg_x[k].extend([i] * trials * N)
                 rep_x[k].extend([i] * trials * N)
                 fit_x[k].extend([i] * trials * N)
+                if vary == 0:
+                    weight.extend([1] * trials * N)
+                else:
+                    weight.extend([1/N] * trials * N)
                 for j in xrange(trials):
                     stg_y[k].extend(data['pst'][j][k])
                     rep_y[k].extend(data['prp'][j][k])
                     fit_y[k].extend(data['pft'][j][k])
     for k in xrange(1, snaps):
         plt.figure()
-        plt.hexbin(rep_x[k], rep_y[k], cmap=cm.binary, gridsize=10)
-    plt.show()
+        plt.hexbin(rep_x[k], rep_y[k], weights[k], cmap=cm.binary, 
+                   gridsize=10, reduce_C_function=sum)
+        plt.savefig('data%d.png' % k, dpi=300)
--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for MEETS in 10 20 50 100 200 500
+for MEETS in 10 20 50 100 200
 do
     echo MEETS=$MEETS
     python reput.py data$MEETS MEETS=$MEETS STEPS=25000 DUMP=2500 C=1\
-    B=5 N=20 PROC=4 SIM=16 
+    B=5 N=$MEETS PROC=4 SIM=16 
 done
 
-python morepics.py data '[10, 20, 50, 100, 200, 500]'
+python morepics.py data '[10, 20, 50, 100, 200]' 1
